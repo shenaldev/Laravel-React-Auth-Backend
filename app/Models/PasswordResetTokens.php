be8e652575a5656397654d988221031b22833dce
Shenal Fernando <developershenal@gmail.com>
2023-06-08 01:36:48 +0530

    remove primary key variable

--- app/Models/PasswordResetTokens.php
+++ app/Models/PasswordResetTokens.php
@@ -7,6 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class PasswordResetTokens extends Model
 {
     protected $table = 'password_reset_tokens';
-    protected $primaryKey = 'email';
     protected $fillable = ['email', 'token'];
 }
